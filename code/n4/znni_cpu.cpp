761b68306b86a97f592a78e15ac1acb6fbad3978
Nico Kemnitz <nkemnitz@princeton.edu>
2016-06-17 14:51:10 +0100

    affinity output for all directions

--- code/n4/znni_cpu.cpp
+++ code/n4/znni_cpu.cpp
@@ -41,26 +41,29 @@ int main(int argc, char *argv[])
   host_tensor<float,5> out_patch(1,3, outsz[0], outsz[1], outsz[2]);
 
   // iterate all the patches
-  for (auto it = dp.begin(); it!=dp.end(); ++it){
+  for (auto it = dp.begin(); it!=dp.end(); ++it) {
     inout = dp.ReadWindowData(*it, to_host);
     for (auto & l: layers){
       inout = l->forward(std::move(inout));
     }
 
-    host_tensor<float, 5> hresult(256, 1, 1, 1, 1);
-    for (long_t i=0; i<256; ++i){
-      hresult[i][0] = inout[i][0];
+    host_tensor<float, 2> hresult(3, 256);
+    for (long_t i = 0; i < 256; ++i) { // TODO: Looks ugly and slow
+      hresult[0][i] = inout[i][0][0][0][0];
+      hresult[1][i] = inout[i][1][0][0][0];
+      hresult[2][i] = inout[i][2][0][0][0];
     }
+
+    host_tensor<float, 5> host_out_patch(1, 3, outsz[0], outsz[1], outsz[2]);
+    host_out_patch[0][0].load_n(ds.deshuffle(hresult[0].data()).data(), 256, from_host);
+    host_out_patch[0][1].load_n(ds.deshuffle(hresult[1].data()).data(), 256, from_host);
+    host_out_patch[0][2].load_n(ds.deshuffle(hresult[2].data()).data(), 256, from_host);
+
     std::cout << "Processing took: " << wt.elapsed<double>() << "\n";
     wt.reset();
 
-    host_array<real> rr = ds.deshuffle(hresult.data());
-    wt.reset();
-
-    out_patch.load(rr.data(), from_host);
     // push to data provider
-    dp.WriteWindowData(*it, out_patch);
-    ////////
+    dp.WriteWindowData(*it, host_out_patch);
     std::cout << "push to data provider: " << wt.elapsed<double>() << "\n";
   }
 }
